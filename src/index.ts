301282afd08ddf582bda482471449591a0257bb4
Ben LaVerriere <benlaverriere@gmail.com>
2021-05-20 12:09:41 -0400

    Tidy output; exclude 'until' date

--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,39 @@
 import yargs from "yargs";
 import * as chrono from "chrono-node";
-import * as dayjs from "dayjs";
+import dayjs from "dayjs";
 
 enum Endpoint {
   From = "from",
   To = "to",
 }
 type ParamSet = {
-  readonly [index in chrono.Component]?: string;
+  readonly [index in dayjs.UnitType]?: string;
 };
 type ParamMap = {
   [index in Endpoint]: ParamSet;
 };
 
 const params: ParamMap = {
-  from: { year: "year", month: "month", day: "day" },
-  to: { year: "y2", month: "m2", day: "d2" },
+  from: { year: "year", month: "month", date: "day" },
+  to: { year: "y2", month: "m2", date: "d2" },
 };
 
 function asURLTuple(
-  date: chrono.ParsedResult,
-  part: chrono.Component,
+  date: dayjs.Dayjs,
+  part: dayjs.UnitType,
   endpoint: Endpoint
 ): string {
-  return `${params[endpoint][part]}=${date.start.get(part)}`;
+  const adjustment = part === "month" ? +1 : 0;
+  return `${params[endpoint][part]}=${date.get(part) + adjustment}`;
 }
 
-function allTuplesFor(date: chrono.ParsedResult, as: Endpoint): string {
-  console.log(as, Object.keys(params[as]));
+function allTuplesFor(date: dayjs.Dayjs, as: Endpoint): string {
   return Object.keys(params[as])
-    .map((part) => asURLTuple(date, part as chrono.Component, as))
+    .map((part) => asURLTuple(date, part as dayjs.UnitType, as))
     .join("&");
 }
 
-function bothEndpoints(
-  from: chrono.ParsedResult,
-  to: chrono.ParsedResult
-): string {
+function bothEndpoints(from: dayjs.Dayjs, to: dayjs.Dayjs): string {
   return [
     allTuplesFor(from, Endpoint.From),
     allTuplesFor(to, Endpoint.To),
@@ -50,8 +47,8 @@ const args = yargs
   })
   .alias({ to: ["until", "u", "end", "e"], from: ["start", "s"] }).argv;
 
-const fromDate = chrono.parse(args.from)[0];
-const toDate = chrono.parse(args.to)[0];
+const fromDate = dayjs(chrono.parseDate(args.from));
+const toDate = dayjs(chrono.parseDate(args.to)).subtract(1, "day");
 
 const baseURL = "https://www.timeanddate.com/calendar/print.html?";
 const formatOptions = "&country=1&typ=4&cols=1&display=1";
@@ -61,5 +58,11 @@ const calendarURL = [
   bothEndpoints(fromDate, toDate),
   formatOptions,
 ].join("");
-console.log("Hello!\n", args);
+const outputFormat = "YYYY-MM-DD";
+
+console.log(
+  `Calendar from ${args.from} (${fromDate.format(outputFormat)}) until ${
+    args.to
+  } (${toDate.format(outputFormat)}):`
+);
 console.log(calendarURL);
